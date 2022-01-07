ab4da9b55559fc3b0d2012379361962a89801b0e
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2022-01-07 17:58:34 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir ="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>reCAPTCHA V2</title>
+        <link rel="stylesheet" href="style.css">
+        <link rel='shortcut icon' type='image/x-icon' href='favicon.ico'/>
+    </head>
+    <body>
+        <h1>Formulário reCAPTCHA V2</h1>
+        <hr>
+    </body>
